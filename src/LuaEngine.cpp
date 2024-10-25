010ce87f510a3aea40357edf75ad7f221a39b219
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-10-25 22:50:50 +0200

    Update LuaEngine.cpp

--- src/LuaEngine.cpp
+++ src/LuaEngine.cpp
@@ -1,6 +1,40 @@
 #include <LuaEngine.h>
 #include <NetworkEngine.h>
 
+/// @brief Because the original structs are not aligned,
+/// we can't access bitfields , so we gotta wrap all
+struct LuaWindivertAddress : WINDIVERT_ADDRESS
+{
+    LuaWindivertAddress() : WINDIVERT_ADDRESS()
+    {
+
+    }
+    LuaWindivertAddress(const WINDIVERT_ADDRESS& other) : WINDIVERT_ADDRESS(other)
+    {
+
+    }
+
+    sol::object get(sol::stack_object key, sol::this_state L)
+    {
+        auto key_str = key.as<sol::optional<std::string>>();
+        std::cout << "got key " << *key_str << std::endl;
+        if (key_str)
+        {
+            const auto str = *key_str;
+            if (str == "Layer")
+            {
+                uint32_t val = this->Layer;
+                return sol::make_object(L, val);
+            } else if (str == "Timestamp")
+            {
+                return sol::make_object(L, this->Timestamp);
+            }
+        }
+        return sol::object(L, sol::in_place, sol::lua_nil);
+
+    }
+};
+
 void LuaEngine::Init()
 {
     _lstate.open_libraries(sol::lib::base);
@@ -8,6 +42,7 @@ void LuaEngine::Init()
 
 
 WINDIVERT_ADDRESS tester;
+LuaWindivertAddress add;
 
 bool LuaEngine::RegisterEnv()
 {
@@ -29,15 +64,16 @@ bool LuaEngine::RegisterEnv()
         }
     );
     auto windivert_meta = _lstate["Windivert"].get_or_create<sol::table>();
-    windivert_meta.new_usertype<WINDIVERT_ADDRESS>("Address",
-        
-        
+    windivert_meta.new_usertype<LuaWindivertAddress>("Address",
+        sol::meta_function::index, &LuaWindivertAddress::get
     );
 
-    tester.Layer |= 0;
+    tester.Layer = 2;
     std::cout << tester.Layer << std::endl;
     tester.Timestamp = INT64_MAX;
-    _lstate["tester"] = std::ref(tester);
+    add = tester;
+    _lstate["tester"] = std::ref(add);
+    std::cout << add.Timestamp << std::endl;
 
     return true;
 }
